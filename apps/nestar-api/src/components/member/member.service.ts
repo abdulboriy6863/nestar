214b54814359585bc3a1bb2a5ab0fb88c11436e1
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-09-25 04:40:22 -0700

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -18,8 +18,8 @@ export class MemberService {
 			//TODO Authentication via TOKEN
 			return result;
 		} catch (err) {
-			console.log('Signup error, Service model:', err);
-			throw new BadRequestException(err);
+			console.log('Signup error, Service model:', err.message);
+			throw new BadRequestException(Message.USED_MEMBERNICK_NICK_OR_PHONE);
 		}
 	}
 
