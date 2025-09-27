2ab9c99d4f9a7241dcb22af937d1c40eba2160bb
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-09-26 19:55:57 -0700

    fix: creation and verification json web token

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -20,6 +20,7 @@ export class MemberService {
 		try {
 			const result = await this.memberModel.create(input);
 			//TODO Authentication via TOKEN
+			result.accessToken = await this.authService.createToken(result);
 			return result;
 		} catch (err) {
 			console.log('Signup error, Service model:', err.message);
@@ -44,8 +45,8 @@ export class MemberService {
 
 		const isMatch = await this.authService.comparePassword(input.memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+		response.accessToken = await this.authService.createToken(response);
 
-		// console.log('response:', response);
 		return response;
 	}
 
