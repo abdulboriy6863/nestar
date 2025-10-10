26d33aa9b25a29bcf355ead2d8505a1fb4208542
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-10-09 23:21:18 -0700

    feat: develop createBoardArticle API

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { Message } from '../../libs/enums/common.enum';
+import { MemberService } from '../member/member.service';
+import { ViewService } from '../view/view.service';
 
 @Injectable()
 export class BoardArticleService {
-	constructor(@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>) {}
+	constructor(
+		@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
+		private readonly memberService: MemberService,
+		private readonly viewService: ViewService,
+	) {}
+
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId; //imputni memberId sini kirib kelgan memberId ga teglashtiryapmiz
+		try {
+			const result = await this.boardArticleModel.create(input);
+			await this.memberService.memberStatsEditor({
+				_id: memberId,
+				targetKey: 'memberArticles',
+				modifier: 1,
+			});
+			//memberArticle statistikasini bittaga oshiradi
+
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+	}
 }
