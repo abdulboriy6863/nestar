094b140fcb0107dc5b1a310d4d625101fa39d012
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-10-10 03:42:24 -0700

    feat: develop comment GraphQL Api

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CommentService } from './comment.service';
+import { UseGuards } from '@nestjs/common';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { ObjectId } from 'mongoose';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Comment } from '../../libs/dto/comment/comment';
 
 @Resolver()
-export class CommentResolver {}
+export class CommentResolver {
+	constructor(private readonly commentService: CommentService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => Comment)
+	public async createComment(
+		@Args('input') input: CommentInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: createComment');
+		return await this.commentService.createComment(memberId, input);
+	}
+}
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -1,4 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { MemberService } from '../member/member.service';
+import { Comment } from '../../libs/dto/comment/comment';
+import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { Message } from '../../libs/enums/common.enum';
+import { PropertyService } from '../property/property.service';
+import { BoardArticleService } from '../board-article/board-article.service';
+import { CommentGroup } from '../../libs/enums/comment.enum';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+	constructor(
+		@InjectModel('Comment') private readonly commentModel: Model<Comment>,
+		private readonly memberService: MemberService,
+		private readonly propertyService: PropertyService,
+		private readonly boardArticleService: BoardArticleService,
+	) {}
+
+	public async createComment(memberId: ObjectId, input: CommentInput): Promise<Comment> {
+		input.memberId = memberId; //imputni memberId sini kirib kelgan memberId ga teglashtiryapmiz
+
+		let result = null;
+		try {
+			result = await this.commentModel.create(input);
+		} catch (err) {
+			console.log('Error, Comment service model:', err.message);
+			throw new BadRequestException(Message.CREATE_FAILED);
+		}
+
+		switch (input.commentGroup) {
+			case CommentGroup.PROPERTY:
+				await this.propertyService.propertyStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'propertyComments',
+					modifier: 1,
+				});
+				break;
+			case CommentGroup.ARTICLE:
+				await this.boardArticleService.boardArticleStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'articleComments',
+					modifier: 1,
+				});
+				break;
+			case CommentGroup.MEMBER:
+				await this.memberService.memberStatsEditor({
+					_id: input.commentRefId,
+					targetKey: 'memberComments',
+					modifier: 1,
+				});
+				break;
+		}
+
+		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+		return result;
+	}
+}
