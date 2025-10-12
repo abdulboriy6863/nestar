d3dba4bf29005fe9beb27701fb2f71590525e93e
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-10-12 07:48:21 -0700

    feat: integration of loopupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -20,6 +20,7 @@ export const availableCommentSorts = ['createdAt', 'updatedAt'];
 /*IMAGE CONFIGURATION*/
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
+import { T } from './types/common';
 
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
@@ -29,6 +30,43 @@ export const getSerialForImage = (filename: string) => {
 
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
+};
+//auth bolgan member.      //propniId            //hosil bolgan property id ni qabul qilish
+export const lookupAuthMemberLiked = (memberId: T, targetRefId: string = '$_id') => {
+	//idRefId mavjud bolmasa shu qiymatni oladi
+	return {
+		$lookup: {
+			//localvariable
+			from: 'likes', //likees collectionda izledi
+			let: {
+				//likes ni ichiga kirib shuni ichidan qidirishini aytyappiz (search)
+				localLikeRefId: targetRefId, //"$_id" bu shunde degani
+				localMemberId: memberId,
+				localMyFavorite: true,
+			},
+			//pipeline
+			pipeline: [
+				{
+					$match: {
+						$expr: {
+							//aynan nimalarni solishtirishimizni belgilaypamiz
+							$and: [{ $eq: ['$likeRefId', '$$localLikeRefId'] }, { $eq: ['$memberId', '$$localMemberId'] }], //ikkalasin teng holatini izla
+							//memberId ni localLikeRefId ga teng bolgan holatin izla
+						},
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						memberId: 1,
+						likeRefId: 1,
+						myFavorite: '$$localMyFavorite',
+					},
+				},
+			],
+			as: 'meLiked',
+		},
+	};
 };
 
 export const lookupMember = {
